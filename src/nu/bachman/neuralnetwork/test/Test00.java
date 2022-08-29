eef5b7ff7786ada07c9d54ffba354c5acb8061fe
Bachman-B <bjarn@bachman.nu>
2022-08-29 20:18:09 +0200

    creating model

---
+++ src/nu/bachman/neuralnetwork/test/Test00.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package nu.bachman.neuralnetwork.test;
+
+/**
+ * @author Björn Bachman
+ *
+ */
+public class Test00 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+
+	}
+
+}
